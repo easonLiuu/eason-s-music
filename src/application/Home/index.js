571aa9a79372d94fc219cbb4e87b917d1c7188c7
liujiarui <ljr3289595084@163.com>
2022-12-23 19:55:49 +0800

    顶部栏开发

--- src/application/Home/index.js
+++ src/application/Home/index.js
@@ -1,14 +1,27 @@
 import React from "react";
 import { renderRoutes } from "react-router-config";
+import { 
+    Top,
+    Tab,
+    TabItem
+} from './style';
+import { NavLink } from 'react-router-dom';// 利用 NavLink 组件进行路由跳转
 
 
 function Home (props) {
     const { route } = props;
     return (
       <div>
-        <div>
-            Home
-        </div>
+        <Top>
+            <span className="iconfont menu">&#xe65c;</span>
+            <span className="title">WebApp</span>
+            <span className="iconfont search">&#xe62b;</span>
+        </Top>
+        <Tab>
+            <NavLink to="/recommend" activeClassName="selected"><TabItem><span > 推荐 </span></TabItem></NavLink>
+            <NavLink to="/singers" activeClassName="selected"><TabItem><span > 歌手 </span></TabItem></NavLink>
+            <NavLink to="/rank" activeClassName="selected"><TabItem><span > 排行榜 </span></TabItem></NavLink>
+        </Tab>
         { renderRoutes (route.routes) }
       </div>
        
